033da30b796001c32d64eeb613451a702bfb7668
RajviDave <rajvidave22@gmail.com>
2026-04-20 22:03:51 +0530

    missing value management

--- EDA.py
+++ EDA.py
@@ -2,4 +2,19 @@ import pandas as pd
 
 dataset=pd.read_csv("Dataset/train.csv")
 
-print(dataset.head())
+# print(dataset.shape)
+# print(dataset.head())
+
+# missing=dataset.isnull()
+# print(missing)
+
+print(dataset['HouseStyle'].isnull()[:25])
+num=[1,2,3,4,5,' ']
+for x in dataset:
+    if x.isnull():
+        print('it has missing values')
+
+
+
+# for features in dataset:
+    
