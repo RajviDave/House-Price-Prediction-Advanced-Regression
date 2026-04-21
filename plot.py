f18e093bcb9b8f7dcf8ac8b12e8dbf2dcce10389
RajviDave <rajvidave22@gmail.com>
2026-04-21 18:45:43 +0530

    plotted

--- plot.py
+++ plot.py
@@ -4,5 +4,8 @@ import numpy as np
 x=np.array([1,2,3,4,5])
 y=np.array([6,4,1,3,8])
 
-plt.plot(x,y)
+plt.plot(x,y,marker="+",linestyle="--",color="b")
+plt.title("Sample Plot")
+
+plt.savefig("plot.pdf",format="pdf")
 plt.show()
